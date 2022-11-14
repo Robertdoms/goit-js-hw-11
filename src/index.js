92a537f727980627b0252d133ac04cc8ba10bf97
RobertDoms <robertdoms90@gmail.com>
2022-11-14 14:52:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,3 +13,36 @@ const DEBOUNCE_DELAY = 500;
 // console.log(document.documentElement.scrollHeight);
 
 window.addEventListener('scroll', debounce(scrollOptions, DEBOUNCE_DELAY));
+
+function scrollOptions() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  //   console.log({ scrollTop, scrollHeight, clientHeight });
+
+  if (clientHeight + scrollTop >= scrollHeight + 5) {
+    // show the loading animation
+    console.log('end of scroll');
+    loadMore();
+  }
+}
+
+window.removeEventListener('scroll', scrollOptions);
+// let infScroll = new InfiniteScroll(refs.gallery, {
+//   // options
+//   path: 'photosApiServices.page + 1',
+//   append: '.post',
+//   history: false,
+//   scrollThreshold: 100,
+// });
+// console.log(infScroll);
+let timerId = null;
+const photosApiServices = new PhotosApiServices();
+// console.log(photosApiServices.page);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('input[name-searchQuery]'),
+  btn: document.querySelector('.submit-btn'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
