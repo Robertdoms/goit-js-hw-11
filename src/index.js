a66109b473d1004af18827e27a8e102b96d9a695
RobertDoms <robertdoms90@gmail.com>
2022-11-14 15:06:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -46,3 +46,47 @@ const refs = {
   btn: document.querySelector('.submit-btn'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
+const loadAnimationAction = document.querySelector('.loader');
+// console.log(loadAnimationAction);
+// console.log(photosApiServices);
+refs.form.addEventListener('submit', onBtnSubmit);
+
+async function onBtnSubmit(e) {
+  e.preventDefault();
+
+  clearGallety();
+  photosApiServices.query = e.currentTarget.elements.searchQuery.value;
+  photosApiServices.resetPage();
+
+  loadAnimationAction.classList.remove('is-hiden');
+  const data = await photosApiServices.fetchPhotos();
+  const markup = data.hits.map(item => itemMarkup(item)).join('');
+  loadAnimationAction.classList.add('is-hiden');
+
+  console.log(data);
+  if (data.total === 0) {
+    btnToTop.classList.add('is-hiden');
+    btnToBot.classList.add('is-hiden');
+    return Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  if (photosApiServices.query === '' || photosApiServices.query === ' ') {
+    btnToTop.classList.add('is-hiden');
+    btnToBot.classList.add('is-hiden');
+
+    return Notify.warning('input field cannot be empty.');
+  } else {
+    renderItem(markup);
+
+    Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    document;
+  }
+}
+
+async function loadMore(e) {
+  loadAnimationAction.classList.remove('is-hiden');
+  const data = await PhotosApiServices.fetchPhotos();
+  const markup = data.hits.map(item => itemMarkup(item)).join('');
+  loadAnimationAction.classList.add('is-hiden');
+}
